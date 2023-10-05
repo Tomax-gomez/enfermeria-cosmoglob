5568d4db1ecf8857f61e05621703659097431713
Tomax-gomez <127521420+Tomax-gomez@users.noreply.github.com>
2023-10-05 09:41:10 -0500

    Add files via upload

---
+++ enfermeriagl/registro/DBinsert.php
@@ -0,0 +1,36 @@
+<?php
+include "DBconnect.php";
+
+if(isset($_POST['insertar'])){
+///////////// Informacion enviada por el formulario /////////////
+$nombres=$_POST['nombres'];
+$apellidos=$_POST['apellidos'];
+$correo=$_POST['correo'];
+$contrasena=$_POST['contraseña'];
+///////// Fin informacion enviada por el formulario ///
+echo $apellidos;
+echo $correo;
+echo $contrasena;
+////////////// Insertar a la tabla la informacion generada ///////
+$sql=("insert into tabla_per (nombre,apellido,correo,contraseña) values(?, ? , ?, ?)");
+echo $sql;
+$sql = $connect->prepare($sql);
+
+$sql->bindParam(1,$nombres);
+$sql->bindParam(2,$apellidos);
+$sql->bindParam(3,$correo);
+$sql->bindParam(4,$contrasena);
+$sql->execute();
+
+$lastInsertId = $connect->lastInsertId();
+if($lastInsertId>0){
+
+
+}
+else{
+echo "<div class='content alert alert-danger'> No se pueden agregar datos, comuníquese con el administrador </div>";
+
+print_r($sql->errorInfo()); 
+}
+}// Cierra envio de guardado
+?>
---
+++ enfermeriagl/registro/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>formulario</title>
+  <link rel="stylesheet" href="../css/styl.css">  
+</head>
+
+<body >
+<?php
+include "DBinsert.php"
+?>
+<section class="form-registrer">
+    <div class="container" > 
+<form class="controls "action="" method="POST">
+  <div >
+    <div >
+
+    <title>FORMULARIO REGISTRO</title>
+</head>
+<body>
+  <section class="form-register">
+    <h4>FORMULARIO REGISTRO</h4>
+    <input class="controls" type="text" name="nombres" id="nombres" placeholder="Ingrese su Nombre">
+    <input class="controls" type="text" name="apellidos" id="apellidos" placeholder="Ingrese su Apellido">
+    <input class="controls" type="email" name="correo" id="correo" placeholder="Ingrese su Correo">
+    <input class="controls" type="password" name="correo" id="correo" placeholder="Ingrese su Contraseña">
+
+    <input class="botons" type="submit" value="Registrar">
+    <p><a href="#">¿Ya tengo Cuenta?</a></p>
+  </section>
+
+            
+<?php   ?>
+</body>
+
+</html>
